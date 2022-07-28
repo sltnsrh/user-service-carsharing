8b4454618eeb36eac19efb537adbe5f662f01da9
sltnsrh <lifenjoy8888@gmail.com>
2022-07-28 14:29:39 +0300

    UserServiceApp initialized, DB connected

---
+++ src/test/java/com/intern/carsharing/UserServiceAppTests.java
@@ -0,0 +1,13 @@
+package com.intern.carsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
