2e57c31eddc419a551ed186bb55931662c6ce80f
sltnsrh <lifenjoy8888@gmail.com>
2022-07-28 16:37:40 +0300

    added UserRepository

---
+++ src/main/java/com/intern/carsharing/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.intern.carsharing.repository;
+
+import com.intern.carsharing.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<AppUser, Long> {
+}
