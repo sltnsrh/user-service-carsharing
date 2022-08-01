91dd3fb897831d18fc427c9a6ab0a7bd41842d01
sltnsrh <lifenjoy8888@gmail.com>
2022-08-01 11:02:36 +0300

    added annotations to RoleRepository and StatusRepository

--- src/main/java/com/intern/carsharing/repository/RoleRepository.java
+++ src/main/java/com/intern/carsharing/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package com.intern.carsharing.repository;
 
 import com.intern.carsharing.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByRoleName(Role.RoleName roleName);
 }
--- src/main/java/com/intern/carsharing/repository/StatusRepository.java
+++ src/main/java/com/intern/carsharing/repository/StatusRepository.java
@@ -2,7 +2,9 @@ package com.intern.carsharing.repository;
 
 import com.intern.carsharing.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StatusRepository extends JpaRepository<Status, Long> {
     Status findByStatusType(Status.StatusType statusType);
 }
