9d6f0fcca8229cb89b888644a1c46b8c654e0626
sltnsrh <lifenjoy8888@gmail.com>
2022-08-03 10:47:22 +0300

    added user login method to the AuthService

--- src/main/java/com/intern/carsharing/service/impl/AuthServiceImpl.java
+++ src/main/java/com/intern/carsharing/service/impl/AuthServiceImpl.java
@@ -2,12 +2,17 @@ package com.intern.carsharing.service.impl;
 
 import com.intern.carsharing.exception.UserAlreadyExistException;
 import com.intern.carsharing.model.User;
+import com.intern.carsharing.model.dto.request.LoginRequestDto;
 import com.intern.carsharing.model.dto.request.RegistrationRequestUserDto;
+import com.intern.carsharing.model.dto.response.LoginResponseDto;
 import com.intern.carsharing.model.dto.response.ResponseUserDto;
 import com.intern.carsharing.service.AuthService;
 import com.intern.carsharing.service.UserService;
 import com.intern.carsharing.service.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +20,7 @@ import org.springframework.stereotype.Service;
 public class AuthServiceImpl implements AuthService {
     private final UserService userService;
     private final UserMapper mapper;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public ResponseUserDto register(RegistrationRequestUserDto requestUserDto) {
@@ -24,6 +30,20 @@ public class AuthServiceImpl implements AuthService {
         }
         User user = mapper.toModel(requestUserDto);
         return mapper.toDto(userService.save(user));
+    }
+
+    @Override
+    public LoginResponseDto login(LoginRequestDto requestDto) {
+        String email = requestDto.getEmail();
+        String password = requestDto.getPassword();
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        User user = userService.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("Can't found user with email: " + email);
+        }
+
+
+        return null;
     }
 
     private boolean userExist(String email) {
