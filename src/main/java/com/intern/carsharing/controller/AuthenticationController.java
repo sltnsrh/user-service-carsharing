c5d3a9806d0477c2656836fd70128997f49e3a3b
sltnsrh <lifenjoy8888@gmail.com>
2022-08-18 16:48:43 +0300

    added swagger annotations

--- src/main/java/com/intern/carsharing/controller/AuthenticationController.java
+++ src/main/java/com/intern/carsharing/controller/AuthenticationController.java
@@ -2,29 +2,70 @@ package com.intern.carsharing.controller;
 
 import com.intern.carsharing.model.dto.request.LoginRequestDto;
 import com.intern.carsharing.model.dto.request.RegistrationRequestUserDto;
+import com.intern.carsharing.model.dto.response.LoginResponseDto;
+import com.intern.carsharing.model.dto.response.ResponseUserDto;
 import com.intern.carsharing.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authentication", description = "Authentication and registration")
 @RestController
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthService authService;
 
+    @Operation(
+            summary = "Registration a new user",
+            description = "Allows to register a new user. "
+                    + "As response user gets a user info with a unique Id.",
+            tags = {"Authentication"},
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Created",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ResponseUserDto.class))
+                    ),
+                    @ApiResponse(responseCode = "400", description = "Bad Request"),
+                    @ApiResponse(responseCode = "409", description = "Conflict")
+            }
+    )
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/registration")
-    public ResponseEntity<Object> register(
+    public ResponseEntity<ResponseUserDto> register(
             @Valid @RequestBody RegistrationRequestUserDto requestUserDto
     ) {
         return new ResponseEntity<>(authService.register(requestUserDto), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "User authentication",
+            description = "Allows to authenticate a user. "
+                    + "As response user gets username and JWT token",
+            tags = {"Authentication"},
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            description = "Ok",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = LoginResponseDto.class))
+                    ),
+                    @ApiResponse(responseCode = "400", description = "Bad Request"),
+                    @ApiResponse(responseCode = "401", description = "Unauthorized")
+            }
+    )
     @PostMapping("/login")
-    public ResponseEntity<Object> login(@Valid @RequestBody LoginRequestDto requestDto) {
+    public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto requestDto) {
         return new ResponseEntity<>(authService.login(requestDto), HttpStatus.OK);
     }
 }
--- src/main/java/com/intern/carsharing/model/dto/response/LoginResponseDto.java
+++ src/main/java/com/intern/carsharing/model/dto/response/LoginResponseDto.java
@@ -1,5 +1,6 @@
 package com.intern.carsharing.model.dto.response;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginResponseDto {
+    @ApiModelProperty(example = "bob@gmail.com")
     private String email;
+    @ApiModelProperty(example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJib2IxQGdtYWlsLmNvbSIsInJvbGV"
+            + "zIjpbIlVTRVIiXSwiaWF0IjoxNjYwNzY0MTgyLCJleHAiOjE2NjA3NjQ1NDJ9.r8kASRvksi3Ev"
+            + "-BS9rQ4L56l_iI7kwIDIy27OIKYp-g")
     private String token;
 }
