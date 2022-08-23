ec9f43b0f7e53cedacdfa3377488be91443fb517
sltnsrh <lifenjoy8888@gmail.com>
2022-08-23 13:04:12 +0300

    added logic for repeat registration

--- src/main/java/com/intern/carsharing/service/impl/AuthServiceImpl.java
+++ src/main/java/com/intern/carsharing/service/impl/AuthServiceImpl.java
@@ -12,6 +12,7 @@ import com.intern.carsharing.model.util.StatusType;
 import com.intern.carsharing.security.jwt.JwtTokenProvider;
 import com.intern.carsharing.service.AuthService;
 import com.intern.carsharing.service.ConfirmationTokenService;
+import com.intern.carsharing.service.StatusService;
 import com.intern.carsharing.service.UserService;
 import com.intern.carsharing.service.mapper.UserMapper;
 import java.time.LocalDateTime;
@@ -32,17 +33,22 @@ public class AuthServiceImpl implements AuthService {
     private final PasswordEncoder encoder;
     private final UserMapper userMapper;
     private final ConfirmationTokenService confirmationTokenService;
+    private final StatusService statusService;
 
     @Override
     public String register(RegistrationRequestUserDto requestUserDto) {
         String email = requestUserDto.getEmail();
-        if (userExist(email)) {
-            throw new UserAlreadyExistException("User with email " + email + " already exists");
+        User user = userService.findByEmail(email);
+        if (user != null) {
+            if (!user.getStatus()
+                    .equals(statusService.findByStatusType(StatusType.INVALIDATE))) {
+                throw new UserAlreadyExistException("User with email " + email + " already exists");
+            }
+            setUserUpdatesAndSave(user, requestUserDto);
+        } else {
+            user = getUserFromDtoWithEncodedPassword(requestUserDto);
         }
-        User user = userMapper.toModel(requestUserDto);
-        user.setPassword(encoder.encode(user.getPassword()));
-        ConfirmationToken confirmationToken = confirmationTokenService
-                .create(userService.save(user));
+        ConfirmationToken confirmationToken = confirmationTokenService.create(user);
         return createResponseMessage(confirmationToken.getToken());
     }
 
@@ -76,8 +82,8 @@ public class AuthServiceImpl implements AuthService {
         if (confirmationToken.getExpiredAt().isBefore(LocalDateTime.now())) {
             throw new ConfirmationTokenInvalidException("Confirmation token was expired.");
         }
-        User user = confirmationToken.getUser();
-        userService.changeStatus(user, StatusType.ACTIVE);
+        confirmationTokenService.setConfirmDate(confirmationToken);
+        User user = userService.changeStatus(confirmationToken.getUser(), StatusType.ACTIVE);
         return userMapper.toDto(user);
     }
 
@@ -91,5 +97,20 @@ public class AuthServiceImpl implements AuthService {
                 + "Confirm your email to activate your account." + System.lineSeparator()
                 + System.lineSeparator()
                 + "localhost:8080/confirm?token=" + token;
+    }
+
+    private User getUserFromDtoWithEncodedPassword(RegistrationRequestUserDto dto) {
+        User user = userMapper.toModel(dto);
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userService.save(user);
+    }
+
+    private void setUserUpdatesAndSave(User user, RegistrationRequestUserDto dto) {
+        user.setPassword(encoder.encode(dto.getPassword()));
+        user.setFirstName(dto.getFirstName());
+        user.setLastName(dto.getLastName());
+        user.setAge(dto.getAge());
+        user.setDriverLicence(dto.getDriverLicence());
+        userService.save(user);
     }
 }
