90da7f0dbfaeecaf02d0d2b219020a40498acbcd
sltnsrh <lifenjoy8888@gmail.com>
2022-08-29 13:28:21 +0300

    token example added

--- src/main/java/com/intern/carsharing/model/dto/request/RefreshTokenRequestDto.java
+++ src/main/java/com/intern/carsharing/model/dto/request/RefreshTokenRequestDto.java
@@ -1,8 +1,10 @@
 package com.intern.carsharing.model.dto.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class RefreshTokenRequestDto {
+    @ApiModelProperty(example = "2903037d-e267-4959-962d-00ca195540df")
     private String token;
 }
