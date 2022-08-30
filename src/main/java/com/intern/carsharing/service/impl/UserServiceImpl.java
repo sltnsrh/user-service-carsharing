baf2eef588ba320cbeef1a06282ef67ea557b433
sltnsrh <lifenjoy8888@gmail.com>
2022-08-30 16:26:23 +0300

    to a UserService added method toBalance

--- src/main/java/com/intern/carsharing/service/impl/UserServiceImpl.java
+++ src/main/java/com/intern/carsharing/service/impl/UserServiceImpl.java
@@ -3,11 +3,14 @@ package com.intern.carsharing.service.impl;
 import com.intern.carsharing.exception.LimitedPermissionException;
 import com.intern.carsharing.exception.UserAlreadyExistException;
 import com.intern.carsharing.exception.UserNotFoundException;
+import com.intern.carsharing.model.Balance;
 import com.intern.carsharing.model.Role;
 import com.intern.carsharing.model.User;
+import com.intern.carsharing.model.dto.request.BalanceRequestDto;
 import com.intern.carsharing.model.dto.request.UserUpdateRequestDto;
 import com.intern.carsharing.model.util.StatusType;
 import com.intern.carsharing.repository.UserRepository;
+import com.intern.carsharing.service.BalanceService;
 import com.intern.carsharing.service.StatusService;
 import com.intern.carsharing.service.UserService;
 import java.util.Objects;
@@ -23,6 +26,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final StatusService statusService;
+    private final BalanceService balanceService;
 
     @Override
     public User findByEmail(String email) {
@@ -87,4 +91,14 @@ public class UserServiceImpl implements UserService {
         user.setStatus(statusService.findByStatusType(statusType));
         return save(user);
     }
+
+    @Override
+    public String toBalance(Long id, BalanceRequestDto balanceRequestDto) {
+        Balance balance = balanceService.findByUserId(id);
+        balance.setValue(balance.getValue().add(balanceRequestDto.getValue()));
+        balanceService.save(balance);
+        return balanceRequestDto.getValue() + " " + balance.getCurrency()
+            + " has been credited to the balance of the user with id " + id;
+    }
+
 }
