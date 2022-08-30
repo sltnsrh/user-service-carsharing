5c64d27bebce0013dabd7ab30f41d150970928e4
sltnsrh <lifenjoy8888@gmail.com>
2022-08-30 16:24:49 +0300

    BalanceRepository added custom query for a findByUserId

--- src/main/java/com/intern/carsharing/repository/BalanceRepository.java
+++ src/main/java/com/intern/carsharing/repository/BalanceRepository.java
@@ -3,9 +3,11 @@ package com.intern.carsharing.repository;
 import com.intern.carsharing.model.Balance;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BalanceRepository extends JpaRepository<Balance, Long> {
-    Optional<Balance> findByUser(Long id);
+    @Query(value = "from Balance where user.id = :id")
+    Optional<Balance> findByUserId(Long id);
 }
