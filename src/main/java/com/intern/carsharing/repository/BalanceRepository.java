8f7079a78eb36703b61e39f1b2c3875180b3a9d4
sltnsrh <lifenjoy8888@gmail.com>
2022-09-05 14:12:02 +0300

    BalanceRepository remove redundant query

--- src/main/java/com/intern/carsharing/repository/BalanceRepository.java
+++ src/main/java/com/intern/carsharing/repository/BalanceRepository.java
@@ -3,11 +3,9 @@ package com.intern.carsharing.repository;
 import com.intern.carsharing.model.Balance;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BalanceRepository extends JpaRepository<Balance, Long> {
-    @Query(value = "from Balance where user.id = :id")
     Optional<Balance> findByUserId(Long id);
 }
