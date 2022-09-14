f9dae65c85b8d9c88bcd8008c00f18fef2d9c53a
sltnsrh <lifenjoy8888@gmail.com>
2022-09-14 18:08:05 +0300

    created CoordinatedDto and fixed the CarRegistrationRequestDto

--- src/main/java/com/intern/carsharing/model/dto/request/CarRegistrationRequestDto.java
+++ src/main/java/com/intern/carsharing/model/dto/request/CarRegistrationRequestDto.java
@@ -1,5 +1,6 @@
 package com.intern.carsharing.model.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -10,31 +11,36 @@ public class CarRegistrationRequestDto {
     @ApiModelProperty(example = "540i")
     private String model;
     @ApiModelProperty(example = "2019")
+    @JsonProperty("constructionYear")
     private int year;
     @ApiModelProperty(example = "80500")
+    @JsonProperty("mileageInKilometers")
     private double millageKm;
     @ApiModelProperty(example = "30.50")
+    @JsonProperty("fuelLevel")
     private double fuelLevelLiter;
     @ApiModelProperty(example = "15.5")
+    @JsonProperty("fuelConsumption")
     private double fuelConsumptionLitersPer100Km;
     @ApiModelProperty(example = "BC1234AB")
     private String licensePlate;
     @ApiModelProperty(example = "sedan")
+    @JsonProperty("carBodyStyle")
     private String bodyType;
     @ApiModelProperty(example = "REAR_WHEEL_DRIVE")
+    @JsonProperty("machineDriveType")
     private String wheelsDriveType;
     @ApiModelProperty(example = "PREMIUM")
     private String carClass;
     @ApiModelProperty(example = "FREE")
+    @JsonProperty("carStatus")
     private String status;
     @ApiModelProperty(example = "gasoline")
     private String engineType;
     @ApiModelProperty(example = "automatic")
     private String transmission;
-    @ApiModelProperty(example = "49.59")
-    private double latitude;
-    @ApiModelProperty(example = "25.92")
-    private double longitude;
+    @ApiModelProperty("\"latitude\": 49.84, \"longitude\": 24.07")
+    private CoordinatesDto coordinates;
     @ApiModelProperty(example = "1")
-    private Long ownerId;
+    private Long carOwnerId;
 }
---
+++ src/main/java/com/intern/carsharing/model/dto/request/CoordinatesDto.java
@@ -0,0 +1,9 @@
+package com.intern.carsharing.model.dto.request;
+
+import lombok.Data;
+
+@Data
+public class CoordinatesDto {
+    private double latitude;
+    private double longitude;
+}
