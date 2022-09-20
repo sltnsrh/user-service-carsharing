c652a7324f69d0fd2f7184e561e95ce93ec64365
sltnsrh <lifenjoy8888@gmail.com>
2022-09-20 15:51:16 +0300

    changeCarStatusDto added json property

--- src/main/java/com/intern/carsharing/model/dto/request/ChangeCarStatusRequestDto.java
+++ src/main/java/com/intern/carsharing/model/dto/request/ChangeCarStatusRequestDto.java
@@ -1,8 +1,10 @@
 package com.intern.carsharing.model.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class ChangeCarStatusRequestDto {
+    @JsonProperty("carStatus")
     private String status;
 }
