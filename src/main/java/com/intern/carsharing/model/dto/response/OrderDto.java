471e56049f7368d5f160b031d79b410c83088d3c
sltnsrh <lifenjoy8888@gmail.com>
2022-09-22 12:15:46 +0300

    OrderDto added date time params

--- src/main/java/com/intern/carsharing/model/dto/response/OrderDto.java
+++ src/main/java/com/intern/carsharing/model/dto/response/OrderDto.java
@@ -1,12 +1,15 @@
 package com.intern.carsharing.model.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import lombok.Data;
 
 @Data
 public class OrderDto {
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime startDateTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime endDateTime;
     private BigDecimal price;
     private Long carId;
