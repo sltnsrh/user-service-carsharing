658a91be118441da6e3d5c8c9d23463a8955a443
sltnsrh <lifenjoy8888@gmail.com>
2022-09-22 12:11:38 +0300

    CarMapper was added

--- src/main/java/com/intern/carsharing/config/SpringConfig.java
+++ src/main/java/com/intern/carsharing/config/SpringConfig.java
@@ -1,6 +1,8 @@
 package com.intern.carsharing.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.intern.carsharing.service.mapper.CarMapper;
+import com.intern.carsharing.service.mapper.CarMapperImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +12,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringConfig {
+
+    @Bean
+    public CarMapper carMapper() {
+        return new CarMapperImpl();
+    }
 
     @Bean
     public ObjectMapper objectMapper() {
