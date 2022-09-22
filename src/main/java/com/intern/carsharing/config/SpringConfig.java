a9ea4cb3cc57419356f280cfe09c5be48da9a788
sltnsrh <lifenjoy8888@gmail.com>
2022-09-22 12:32:21 +0300

    CarMapper bean creation was fixed

--- src/main/java/com/intern/carsharing/config/SpringConfig.java
+++ src/main/java/com/intern/carsharing/config/SpringConfig.java
@@ -1,8 +1,6 @@
 package com.intern.carsharing.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.intern.carsharing.service.mapper.CarMapper;
-import com.intern.carsharing.service.mapper.CarMapperImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,11 +10,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SpringConfig {
-
-    @Bean
-    public CarMapper carMapper() {
-        return new CarMapperImpl();
-    }
 
     @Bean
     public ObjectMapper objectMapper() {
--- src/main/java/com/intern/carsharing/service/mapper/CarMapper.java
+++ src/main/java/com/intern/carsharing/service/mapper/CarMapper.java
@@ -3,11 +3,9 @@ package com.intern.carsharing.service.mapper;
 import com.intern.carsharing.model.dto.response.CarDto;
 import com.intern.carsharing.model.dto.response.CarStatisticsResponseDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface CarMapper {
-    CarMapper INSTANCE = Mappers.getMapper(CarMapper.class);
 
     CarStatisticsResponseDto toStatisticsDto(CarDto carDto);
 }
