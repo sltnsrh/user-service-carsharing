f0136568d73e2422263559e92bc3c11208e37fcc
sltnsrh <lifenjoy8888@gmail.com>
2022-09-28 10:11:25 +0300

    getTripStatistics and getCarStatistics tests created for UserServiceImpl

--- src/test/java/com/intern/carsharing/service/impl/UserServiceImplTest.java
+++ src/test/java/com/intern/carsharing/service/impl/UserServiceImplTest.java
@@ -2,6 +2,7 @@ package com.intern.carsharing.service.impl;
 
 import static org.mockito.ArgumentMatchers.any;
 
+import com.intern.carsharing.exception.CarNotFoundException;
 import com.intern.carsharing.model.Balance;
 import com.intern.carsharing.model.Status;
 import com.intern.carsharing.model.User;
@@ -12,15 +13,24 @@ import com.intern.carsharing.repository.UserRepository;
 import com.intern.carsharing.service.BalanceService;
 import com.intern.carsharing.service.PermissionService;
 import com.intern.carsharing.service.StatusService;
+import com.intern.carsharing.service.mapper.CarMapper;
+import com.intern.carsharing.service.mapper.CarMapperImpl;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Optional;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
@@ -34,6 +44,11 @@ class UserServiceImplTest {
     private UserRepository userRepository;
     @Mock
     private StatusService statusService;
+    @Spy
+    private final CarMapper carMapper = new CarMapperImpl();
+    @Spy
+    private final WebClient backofficeServiceClient = WebClient.create("http://localhost:8082");
+    private final MockWebServer mockWebServer = new MockWebServer();
 
     @Test
     void updateWithValidUser() {
@@ -103,5 +118,167 @@ class UserServiceImplTest {
         String actual = userService.fromBalance(1L, balanceRequestDto);
         Assertions.assertEquals("Not enough money on balance for a transaction",
                 actual);
+    }
+
+    @Test
+    void getTripStatisticsWithValidUserIdAndParameters() throws IOException {
+        mockWebServer.start(8082);
+        mockWebServer.enqueue(new MockResponse()
+                .setResponseCode(200)
+                .addHeader("Content-type", "application/json")
+                .setBody("{\"trip\":\"1\"}"));
+
+        Object actual = userService.getTripStatistics(
+                1L,
+                "2022-01-01",
+                "2022-02-01",
+                "PREMIUM"
+        );
+        mockWebServer.shutdown();
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals("{trip=1}", actual.toString());
+    }
+
+    @Test
+    void getTripStatisticsWithValidUserId() throws IOException {
+        mockWebServer.start(8082);
+        mockWebServer.enqueue(new MockResponse()
+                .setResponseCode(200)
+                .addHeader("Content-type", "application/json")
+                .setBody("{\"trip\":\"1\"}"));
+
+        Object actual = userService.getTripStatistics(1L, null, null, null);
+        mockWebServer.shutdown();
+        Assertions.assertNotNull(actual);
+        Assertions.assertEquals("{trip=1}", actual.toString());
+    }
+
+    @Test
+    void getCarStatisticsWithValidUserIdAndCarId() throws IOException {
+        mockWebServer.start(8082);
+        mockWebServer.enqueue(new MockResponse()
+                .setResponseCode(200)
+                .addHeader("Content-type", "application/json")
+                .setBody("{\n"
+                        + "    \"id\": 1,\n"
+                        + "    \"brand\": \"Renault\",\n"
+                        + "    \"model\": \"Megan 8\",\n"
+                        + "    \"constructionYear\": 2020,\n"
+                        + "    \"mileageKm\": 18001.0,\n"
+                        + "    \"fuelLevelLiter\": 30.3,\n"
+                        + "    \"fuelConsumptionLitersPer100Km\": 7.5,\n"
+                        + "    \"licensePlate\": \"ВС1221СР\",\n"
+                        + "    \"carBodyStyle\": \"SEDAN\",\n"
+                        + "    \"carClass\": \"MEDIUM\",\n"
+                        + "    \"carStatus\": \"LOCKED\",\n"
+                        + "    \"engineType\": \"DIESEL\",\n"
+                        + "    \"machineDriveType\": \"FRONT_WHEEL_DRIVE\",\n"
+                        + "    \"transmission\": \"MANUAL_TRANSMISSION\",\n"
+                        + "    \"carOwnerId\": 1,\n"
+                        + "    \"coordinates\": {\n"
+                        + "        \"latitude\": 49.84,\n"
+                        + "        \"longitude\": 24.07\n"
+                        + "    }\n"
+                        + "}"));
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200)
+                .setHeader("Content-type", "application/json")
+                .setBody("[\n"
+                        + "        {\n"
+                        + "            \"startDateTime\": \"2021-11-10 00:00:00\",\n"
+                        + "            \"endDateTime\": \"2021-11-13 00:00:00\",\n"
+                        + "            \"price\": 1500,\n"
+                        + "            \"carId\": 1\n"
+                        + "        },\n"
+                        + "        {\n"
+                        + "            \"startDateTime\": \"2022-01-02 00:00:00\",\n"
+                        + "            \"endDateTime\": \"2022-01-03 00:00:00\",\n"
+                        + "            \"price\": 700,\n"
+                        + "            \"carId\": 1\n"
+                        + "        }\n"
+                        + "    ]"));
+
+        ResponseEntity<Object> actual = userService.getCarStatistics(
+                1L, 1L, null, null, null);
+        mockWebServer.shutdown();
+        Assertions.assertEquals(HttpStatus.OK, actual.getStatusCode());
+    }
+
+    @Test
+    void getCarStatisticsWithInvalidCarId() throws IOException {
+        mockWebServer.start(8082);
+        mockWebServer.enqueue(new MockResponse()
+                .setResponseCode(404));
+        Assertions.assertThrows(CarNotFoundException.class,
+                () -> userService.getCarStatistics(
+                        1L, 1L, null, null, null));
+        mockWebServer.shutdown();
+    }
+
+    @Test
+    void getCarStatisticsWithValidUserIdAndCarIdAndNoOrders() throws IOException {
+        mockWebServer.start(8082);
+        mockWebServer.enqueue(new MockResponse()
+                .setResponseCode(200)
+                .addHeader("Content-type", "application/json")
+                .setBody("{\n"
+                        + "    \"id\": 1,\n"
+                        + "    \"brand\": \"Renault\",\n"
+                        + "    \"model\": \"Megan 8\",\n"
+                        + "    \"constructionYear\": 2020,\n"
+                        + "    \"mileageKm\": 18001.0,\n"
+                        + "    \"fuelLevelLiter\": 30.3,\n"
+                        + "    \"fuelConsumptionLitersPer100Km\": 7.5,\n"
+                        + "    \"licensePlate\": \"ВС1221СР\",\n"
+                        + "    \"carBodyStyle\": \"SEDAN\",\n"
+                        + "    \"carClass\": \"MEDIUM\",\n"
+                        + "    \"carStatus\": \"LOCKED\",\n"
+                        + "    \"engineType\": \"DIESEL\",\n"
+                        + "    \"machineDriveType\": \"FRONT_WHEEL_DRIVE\",\n"
+                        + "    \"transmission\": \"MANUAL_TRANSMISSION\",\n"
+                        + "    \"carOwnerId\": 1,\n"
+                        + "    \"coordinates\": {\n"
+                        + "        \"latitude\": 49.84,\n"
+                        + "        \"longitude\": 24.07\n"
+                        + "    }\n"
+                        + "}"));
+        mockWebServer.enqueue(new MockResponse().setResponseCode(200)
+                .setHeader("Content-type", "application/json"));
+        ResponseEntity<Object> actual = userService.getCarStatistics(
+                1L, 1L, null, null, null);
+        mockWebServer.shutdown();
+        Assertions.assertEquals(HttpStatus.OK, actual.getStatusCode());
+    }
+
+    @Test
+    void getCarStatisticsWithValidUserIdAndCarNotBelongUser() throws IOException {
+        mockWebServer.start(8082);
+        mockWebServer.enqueue(new MockResponse()
+                .setResponseCode(200)
+                .addHeader("Content-type", "application/json")
+                .setBody("{\n"
+                        + "    \"id\": 1,\n"
+                        + "    \"brand\": \"Renault\",\n"
+                        + "    \"model\": \"Megan 8\",\n"
+                        + "    \"constructionYear\": 2020,\n"
+                        + "    \"mileageKm\": 18001.0,\n"
+                        + "    \"fuelLevelLiter\": 30.3,\n"
+                        + "    \"fuelConsumptionLitersPer100Km\": 7.5,\n"
+                        + "    \"licensePlate\": \"ВС1221СР\",\n"
+                        + "    \"carBodyStyle\": \"SEDAN\",\n"
+                        + "    \"carClass\": \"MEDIUM\",\n"
+                        + "    \"carStatus\": \"LOCKED\",\n"
+                        + "    \"engineType\": \"DIESEL\",\n"
+                        + "    \"machineDriveType\": \"FRONT_WHEEL_DRIVE\",\n"
+                        + "    \"transmission\": \"MANUAL_TRANSMISSION\",\n"
+                        + "    \"carOwnerId\": 2,\n"
+                        + "    \"coordinates\": {\n"
+                        + "        \"latitude\": 49.84,\n"
+                        + "        \"longitude\": 24.07\n"
+                        + "    }\n"
+                        + "}"));
+        Assertions.assertThrows(CarNotFoundException.class,
+                () -> userService.getCarStatistics(
+                1L, 1L, null, null, null));
+        mockWebServer.shutdown();
     }
 }
