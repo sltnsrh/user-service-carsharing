26b65049c66ec263b9457a025adba10e1150a9d5
sltnsrh <lifenjoy8888@gmail.com>
2022-10-04 10:39:20 +0300

    RefreshTokenServiceImpl test was added

--- src/test/java/com/intern/carsharing/service/impl/RefreshTokenServiceImplTest.java
+++ src/test/java/com/intern/carsharing/service/impl/RefreshTokenServiceImplTest.java
@@ -1,8 +1,13 @@
 package com.intern.carsharing.service.impl;
 
+import static org.mockito.ArgumentMatchers.any;
+
 import com.intern.carsharing.exception.RefreshTokenException;
 import com.intern.carsharing.model.RefreshToken;
+import com.intern.carsharing.model.User;
 import com.intern.carsharing.repository.RefreshTokenRepository;
+import com.intern.carsharing.service.UserService;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,22 +19,42 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class RefreshTokenServiceImplTest {
+    private static final String token = "refresh-token";
     @InjectMocks
     private RefreshTokenServiceImpl refreshTokenService;
     @Mock
     private RefreshTokenRepository refreshTokenRepository;
+    @Mock
+    private UserService userService;
 
     @Test
     void getByTokenWithExistToken() {
-        Mockito.when(refreshTokenRepository.findByToken("token"))
+        Mockito.when(refreshTokenRepository.findByToken(token))
                 .thenReturn(Optional.of(new RefreshToken()));
-        RefreshToken actual = refreshTokenService.findByToken("token");
+        RefreshToken actual = refreshTokenService.findByToken(token);
         Assertions.assertNotNull(actual);
     }
 
     @Test
     void getByTokenWithNotExistToken() {
         Assertions.assertThrows(RefreshTokenException.class,
-                () -> refreshTokenService.findByToken("token"));
+                () -> refreshTokenService.findByToken(token));
+    }
+
+    @Test
+    void createRefreshTokenWithExistingSameInDb() {
+        User user = new User();
+        Mockito.when(userService.get(1L)).thenReturn(user);
+        Mockito.when(refreshTokenRepository.findByToken(any(String.class)))
+                .thenReturn(Optional.of(new RefreshToken()))
+                .thenReturn(Optional.empty());
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setToken(token);
+        refreshToken.setId(1L);
+        refreshToken.setExpiredAt(LocalDateTime.now().plusMinutes(5));
+        refreshToken.setUser(user);
+        Mockito.when(refreshTokenRepository.save(any(RefreshToken.class))).thenReturn(refreshToken);
+        RefreshToken actual = refreshTokenService.create(1L);
+        Assertions.assertEquals(token, actual.getToken());
     }
 }
