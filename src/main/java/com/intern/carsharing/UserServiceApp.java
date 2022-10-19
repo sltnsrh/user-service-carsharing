9f245f33c0aaf08d677fa4ba120b86cd51657ba8
sltnsrh <lifenjoy8888@gmail.com>
2022-10-19 12:23:13 +0300

    added eureka client

--- src/main/java/com/intern/carsharing/UserServiceApp.java
+++ src/main/java/com/intern/carsharing/UserServiceApp.java
@@ -2,10 +2,12 @@ package com.intern.carsharing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApp {
 
     public static void main(String[] args) {
