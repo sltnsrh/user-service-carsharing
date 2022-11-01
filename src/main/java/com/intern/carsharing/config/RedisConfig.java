2dd03a3885e6cc93a202d538f8e8d23fe907050f
sltnsrh <lifenjoy8888@gmail.com>
2022-11-01 16:22:44 +0200

    redis config fixed

--- src/main/java/com/intern/carsharing/config/RedisConfig.java
+++ src/main/java/com/intern/carsharing/config/RedisConfig.java
@@ -1,16 +1,24 @@
 package com.intern.carsharing.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String hostName;
+    @Value("${spring.redis.port}")
+    private int port;
 
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration =
+                new RedisStandaloneConfiguration(hostName, port);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
